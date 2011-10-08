755cd9adf4f184b96a0c9de38dd033e5ccb8c151
Stephen Mann <thanthese@gmail.com>
2011-10-08 16:59:05 -0500

    removed default welcome page

--- src/ffcop/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns ffcop.views.welcome
-  (:require [ffcop.views.common :as common])
-  (:use noir.core
-        hiccup.core
-        hiccup.page-helpers))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to ffcop"]))
