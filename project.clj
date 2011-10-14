a2edfc49cd598d05cea3e0d190ea9d7f019a55c6
Stephen Mann <thanthese@gmail.com>
2011-10-13 22:21:10 -0500

    including the latest hiccup doesn't seem to be necessary

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.0.6"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
-                 [noir "1.2.0"]
-                 [hiccup "0.3.6"]]
+                 [noir "1.2.0"]]
   :dev-dependencies [[lein-ring "0.4.6"]]
   :ring {:handler ffcop.server/handler}
   :main ffcop.server)
