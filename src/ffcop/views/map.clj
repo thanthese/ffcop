84e50426c4ab243972fb7e0f806db1a948b1483d
Stephen Mann <thanthese@gmail.com>
2011-10-17 09:34:55 -0500

    only what works in map

--- src/ffcop/views/map.clj
+++ src/ffcop/views/map.clj
@@ -12,14 +12,14 @@
   (html5
     [:head
      [:title "ffcop map"]
-     (include-js "ext-3.4.0/adapter/ext/ext-base.js"
-                 "ext-3.4.0/ext-all.js"
-                 "OpenLayers-2.11/OpenLayers.js")
-     (include-css "GeoExt/resources/css/geoext-all-debug.css"
-                  "ext-3.4.0/resources/css/ext-all.css")
-     (include-js "GeoExt/lib/GeoExt.js"
+     (include-js "/ext-3.4.0/adapter/ext/ext-base.js"
+                 "/ext-3.4.0/ext-all.js"
+                 "/OpenLayers-2.11/OpenLayers.js")
+     (include-css "/GeoExt/resources/css/geoext-all-debug.css"
+                  "/ext-3.4.0/resources/css/ext-all.css")
+     (include-js "/GeoExt/lib/GeoExt.js"
                  "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"
-                 "js/map.js")]
+                 "/js/map.js")]
     [:body
      [:div#gxmap]
      [:div#layerlist]]))
